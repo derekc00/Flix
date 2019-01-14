866ba8eb1cd71936dcae109a470e6f6285fa10a0
Derek <derekchang@dhcp-10-8-055-035.mobile.reshsg.uci.edu>
2019-01-13 16:34:44 -0800

    Unit 1 complete with attempt at autolayout for different devices as well as portrait and landscape layouts

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Derek Chang on 1/12/19.
+//  Copyright © 2019 Derek Chang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
